4185bcdb102f6031ec43ff6ba62e992d0fbb8f26
Computer <hal@hals-MacBook-Pro.local>
2014-10-01 15:26:53 -0400

    another commit

--- LionsAndTIgers/Tiger.swift
+++ LionsAndTIgers/Tiger.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 struct Tiger {
     var age = 0
--- LionsAndTIgers/ViewController.swift
+++ LionsAndTIgers/ViewController.swift
@@ -15,9 +15,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var breedLabel: UILabel!
     
+    var myTigers:[Tiger] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var myTiger = Tiger()
+        myTiger.name = "Shere Khan"
+        myTiger.breed = "Siberian Tiger"
+        myTiger.age = 17
+        myTiger.image = UIImage(named:"SiberianTiger.jpg")
+        
+        myTigers.append(myTiger)
+        
+        println("My tiger's name is \(myTiger.name). His breed is \(myTiger.breed), and his image is \(myTiger.image).")
+        
+        myImageView.image = myTiger.image
+        nameLabel.text = myTiger.name
+        ageLabel.text = "\(myTiger.age)"
+        breedLabel.text = myTiger.breed
+        
+        var secondTiger = Tiger()
+        secondTiger.name = "Tigris"
+        secondTiger.breed = "Bengal Tiger"
+        secondTiger.age = 5
+        secondTiger.image = UIImage(named: "BengalTiger.jpg")
+        
+        var thirdTiger = Tiger()
+        thirdTiger.name = "Killa"
+        thirdTiger.breed = "Indochinese Tiger"
+        thirdTiger.age = 9
+        thirdTiger.image = UIImage(named:"IndochineseTiger.jpg")
+        
+        var fourthTiger = Tiger()
+        fourthTiger.name = "Mali"
+        fourthTiger.breed = "Malay Tiger"
+        fourthTiger.age = 4
+        fourthTiger.image = UIImage(named: "MalayanTiger.jpg")
+        
+        myTigers += [secondTiger, thirdTiger, fourthTiger]
+        println("\(myTigers)")
     }
 
     override func didReceiveMemoryWarning() {

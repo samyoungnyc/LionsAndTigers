3cbfb765b800358eb4e81bf9df236555e6355acf
Computer <hal@hals-MacBook-Pro.local>
2014-10-01 12:36:22 -0400

    first commit

---
+++ LionsAndTIgers/Tiger.swift
@@ -0,0 +1,16 @@
+//
+//  Tiger.swift
+//  LionsAndTIgers
+//
+//  Created by Computer on 10/1/14.
+//  Copyright (c) 2014 Computer. All rights reserved.
+//
+
+import Foundation
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var  breed = ""
+    var image = UIImage(named:"")
+}
--- LionsAndTIgers/ViewController.swift
+++ LionsAndTIgers/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +26,7 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func nextBarButtonPressed(sender: UIBarButtonItem) {
+    }
 }
 
